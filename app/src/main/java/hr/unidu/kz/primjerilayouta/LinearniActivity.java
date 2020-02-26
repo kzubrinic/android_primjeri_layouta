a8625195316258cc79cb46018208517e3ef8af21
Kruno <kruno.zubrinic@gmail.com>
2020-02-26 11:41:56 +0100

    Prijelaz na Android 10

--- app/src/main/java/hr/unidu/kz/primjerilayouta/LinearniActivity.java
+++ app/src/main/java/hr/unidu/kz/primjerilayouta/LinearniActivity.java
@@ -1,13 +1,13 @@
 package hr.unidu.kz.primjerilayouta;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ConstraintActivity extends AppCompatActivity {
+public class LinearniActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_constraint);
+        setContentView(R.layout.activity_linearni);
     }
 }
